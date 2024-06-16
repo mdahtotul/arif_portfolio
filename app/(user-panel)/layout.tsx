061704122a3ddb49e1cfc1a197a0e68c9300164a
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-06-17 01:37:38 +0600

    feat: project setup and color added done

---
+++ app/(user-panel)/layout.tsx
@@ -0,0 +1,9 @@
+import HomePage from "./page";
+
+export default function HomePageLayout() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
---
+++ app/(user-panel)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="h-full w-full bg-primary_orange dark:bg-primary_orange">
+      HomePage
+    </div>
+  );
+}
