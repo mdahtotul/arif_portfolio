408d88b776de2699141e75e2af0461b20267b71c
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-06-20 19:49:21 +0600

    feat: another responsive section done

--- components/HomePortion/HeroSection.tsx
+++ components/HomePortion/HeroSection.tsx
@@ -6,22 +6,27 @@ import { TextGenerateEffect } from "../ui/TextGenerateEffect";
 import HeroSpotlights from "./HeroSpotlights";
 
 export default function HeroSection() {
-  const handleCheckResume = () => {};
+  const handleCheckResume = () => {
+    window.open(
+      "https://drive.google.com/file/d/1H7q0O2Z1qOB845CEv1ANX7eqtsCS8zyy/view?usp=sharing",
+      "_blank"
+    );
+  };
   return (
     <>
       <section className="h-[700px] ">
         <HeroSpotlights />
 
         <div className="">
-          {/* <div className="w-[`90%] md:max-w-3xl mx-auto mt-14">
+          <div className="w-[`90%] md:max-w-3xl mx-auto mt-14">
             <TextGenerateEffect
               words="Want Seamless Experiences with Your Website?"
               className="font-title font-normal text-4xl xs:text-5xl md:text-7xl text-primary_black dark:text-white text-center"
               spanWords={["Seamless", "Experiences"]}
               spanClassName="text-secondary_orange"
             />
-          </div>` */}
-
+          </div>
+          `
           <p className="font-subtitle max-w-xl text-center mx-auto text-secondary_gray text-base sm:text-xl font-medium mt-7">
             If you seek smooth experiences, stunning visuals, and
             industry-standard code quality for your website, you&apos;re in the
@@ -30,7 +35,12 @@ export default function HeroSection() {
         </div>
 
         <div className="flex justify-center gap-4 mt-10">
-          <Button2 type="button" variant="orange" size="lg">
+          <Button2
+            type="button"
+            variant="orange"
+            size="lg"
+            onClick={handleCheckResume}
+          >
             Check Resume
           </Button2>
           <Button2 type="button" variant="black" size="lg">
