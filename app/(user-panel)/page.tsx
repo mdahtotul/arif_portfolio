84b360d39882d810d008ead3acda57529db1ee03
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2024-06-20 20:26:49 +0600

    fix: skill slider added

--- app/(user-panel)/page.tsx
+++ app/(user-panel)/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     <div className="max-w-7xl w-full h-full bg-primary_white dark:bg-primary_black pb-20  mx-auto overflow-x-hidden">
       <HeroSection />
 
-      {/* <SkillSection /> */}
+      <SkillSection />
 
       {/* <ProjectSection /> */}
     </div>
